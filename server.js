07ca57445e5cc73bd500ddde35a9b93dca993ff0
Georgia Glassie <georgiaglassie@gmail.com>
2015-10-02 22:14:28 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+////  server to get georgiaglassie.com up and running. using the node.js book as a guide to figuring out node ////
+var express = require('express');
+
+var app = express();
+
+//set up handlebars view engine
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 8000); 
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+//404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+//500 catch-all handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.send('500 - Server Error :-( ');
+});
+
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
+	console.log('\n\n\nYO\n\n\n');
+
+});
